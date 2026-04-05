add5d89509e77220bfce1a64f47da7c616643b40
kimwersen1 <137406910+kimwersen1@users.noreply.github.com>
2026-04-05 11:52:17 +0100

    Update and rename _worker.js to .assetsignore

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    let path = url.pathname;
-    if (path === '/') path = '/index.html';
-    if (!path.includes('.')) path = path + '.html';
-    return env.ASSETS.fetch(new Request(new URL(path, url), request));
-  }
-};
